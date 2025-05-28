2abb8da0c3c1c761e1eee9e0ebfcf5e6add84e44
marjorieisabel <amourakayleigh@gmail.com>
2025-05-28 17:50:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 roasts = [
     "Si {username} yang katanya mau LRP dari tahun lalu tapi kok sampai sekarang masih aktif? Belum siap kehilangan temen virtual atau emang masih belum nemu kegiatan di real life?",
     "Kamu, iya kamu {username} yang suka on tengah malem cuma buat diving mention tab atau upchar. Aslinya emang karena gabut atau emang karena gak ada yang ucapin good night?",
-    "{username}? Stop nambah akun melulu, cabang kamu udah banyak. Mau ngalahin cabang Mie Gacoan?",
+    "{username}? Stop nambah akun terus, cabang kamu udah banyak. Mau ngalahin cabang Mie Gacoan?",
     "{username} udahan jadi RP loyo, itu kasihan mention tab masih numpuk kayak tumpukan baju yang gak dilaundry selama 2 minggu.",
     "Ganti bio/layout tiap minggu, tapi perasaan malah masih stuck di masa lalu. {username}, waktunya buat upgrade perasaan juga biar gak ngestuck di situ-situ aja.",
-    "Posting new tweet mulu, tapi mention sebelumnya gak pernah dibalas. {username} suka banget numpuk mention, nanti pas mau diving mentab malah pusing sendiri karena numpuk.",
+    "Posting new tweet terus, tapi mention sebelumnya gak pernah dibalas. {username} suka banget numpuk mention, nanti pas mau diving mentab malah pusing sendiri karena numpuk.",
     "{username} yang doyan banget mention temen lain di tweet, aura kesepian kamu langsung kelihatan. Padahal temennya juga belum tentu balas.",
     "Eh kamu, {username} yang tiap hari lihat kemesraan orang lain di timeline. Udah waktunya giliran kamu yang pamer kemesraan biar gak jadi nyamuk timeline.",
     "Suka nyabang akun baru berkedok new life, akhirnya ketauan juga karena vibes dan typing masih sama kayak sebelumnya. Mau nyabang berapa banyak akun lagi, {username}?",
